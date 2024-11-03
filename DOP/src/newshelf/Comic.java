3e52ea4f903860df44cb59527d0ab4a80cd2b77a
nivin2004 <115636503+nivin2004@users.noreply.github.com>
2024-11-03 12:51:14 +0530

    Update Comic.java

--- DOP/src/newshelf/Comic.java
+++ DOP/src/newshelf/Comic.java
@@ -1,8 +1,19 @@
 package newshelf;
 
-public record Comic(String title, int ageOfMainCharacter) implements IBook {
+public record Comic(String title, int ageRating) implements IBook<String> {
+
     @Override
     public String getTitle() {
         return title;
+    }
+
+    @Override
+    public int compareTo(IBook<String> other) {
+        // Compare based on the title of the Comic
+        return this.title.compareTo(other.getTitle());
+    }
+
+    public int ageRating() {
+        return ageRating;
     }
 }
