01e7122ac708946e74bd565b3c59a51ea32a6458
nivin2004 <115636503+nivin2004@users.noreply.github.com>
2024-11-03 12:50:21 +0530

    Update Fiction.java

--- DOP/src/newshelf/Fiction.java
+++ DOP/src/newshelf/Fiction.java
@@ -1,27 +1,14 @@
-package oldshelf;
+package newshelf;
 
-public class Fiction extends Book {
+public record Fiction(String name) implements IBook<String> {
 
-    private final String name;
-
-    private final FictionType type;
-
-    public Fiction(String name, FictionType type) {
-        super(name);
-        this.name = name;
-        this.type = type;
-    }
-
-    public String getName() {
+    @Override
+    public String getTitle() {
         return name;
     }
 
-    public FictionType getType() {
-        return type;
-    }
-
     @Override
-    public String toString() {
-        return "Fiction{name='" + name + "', type=" + type + "}";
+    public int compareTo(IBook<String> other) {
+        return this.name.compareTo(other.getTitle());
     }
 }
